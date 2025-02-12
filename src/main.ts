26d587e20ec94997c0becfd9aba5e2926f000774
Gustavo Sandrin <94086373+gustavossandrin@users.noreply.github.com>
2025-02-12 09:01:39 -0300

    able cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const expressAdapter = new ExpressAdapter(server);
   app = await NestFactory.create(AppModule, expressAdapter);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://kanbanlife-front.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
